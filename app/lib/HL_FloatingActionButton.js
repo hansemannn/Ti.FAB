d770d0b5b8a5f9f36f97d7cf323b7d8dbcfc7784
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-08-05 11:33:20 +0200

    chore: use ES6

--- app/lib/HL_FloatingActionButton.js
+++ app/lib/HL_FloatingActionButton.js
@@ -1,83 +1,75 @@
-var FloatingActionButton = require('com.getbase.floatingactionbutton.FloatingActionButton'),
-    FloatingActionMenu = require('com.getbase.floatingactionbutton.FloatingActionsMenu'),
-    Color = require('android.graphics.Color'),
-    Resources = require('android.content.res.Resources'),
-    resourcesReference = null;
-    id = 0;
+import { FloatingActionButton, FloatingActionsMenu } from 'com.getbase.floatingactionbutton.*';
+import Color from 'android.graphics.Color';
+import Activity from 'android.app.Activity';
 
-exports.createFloatingActionButton = function(args) {
-    if (!args.activity) {
-        throw "You need a reference of your Activity in order to create a FAB";
-    }
+const activity = new Activity(Ti.Android.currentActivity);
+let id = 0;
 
-    var fab = new FloatingActionButton(args.activity);
+exports.createFloatingActionButton = function (args) {
+	const fab = new FloatingActionButton(activity);
 
-    // Assigns an incremental ID to the Floating Action Button so identifying the source of clicks is easier
-    fab.setId(id++);
+	// Assigns an incremental ID to the Floating Action Button so identifying the source of clicks is easier
+	fab.setId(id++);
 
-    // Changes the size of the Floating Action Button
-    args.size && exports.setSize(fab, args.size);
+	// Changes the size of the Floating Action Button
+	args.size && exports.setSize(fab, args.size);
 
-    // Changes the normal color
-    args.normalColor && exports.setColorNormal(fab, args.normalColor);
+	// Changes the normal color
+	args.normalColor && exports.setColorNormal(fab, args.normalColor);
 
-    // Changes the pressed color
-    args.pressedColor && exports.setColorPressed(fab, args.pressedColor);
+	// Changes the pressed color
+	args.pressedColor && exports.setColorPressed(fab, args.pressedColor);
 
-    // Changes the disabled color
-    args.disabledColor && exports.setColorDisabled(fab, args.disabledColor);
+	// Changes the disabled color
+	args.disabledColor && exports.setColorDisabled(fab, args.disabledColor);
 
-    // Sets an icon for the Button
-    args.icon && exports.setIcon(fab, args.icon, args.activity);
+	// Sets an icon for the Button
+	args.icon && exports.setIcon(fab, args.icon, activity);
 
-    // Sets an OnClickListener to the Button
-    args.onClick && fab.setOnClickListener(args.onClick);
+	// Sets an OnClickListener to the Button
+	args.onClick && fab.setOnClickListener(args.onClick);
 
-    return fab;
+	return fab;
 };
 
-exports.createFloatingActionMenu = function(args) {
-    if (!args.activity) {
-        throw "You need a reference of your Activity in order to create a FAB";
-    }
+exports.createFloatingActionMenu = function (args) {
+	const fam = new FloatingActionsMenu(activity);
 
-    var fam = new FloatingActionMenu(args.activity);
+	// Assigns an incremental ID to the Floating Action Button so identifying the source of clicks is easier
+	fam.setId(id++);
 
-    // Assigns an incremental ID to the Floating Action Button so identifying the source of clicks is easier
-    fam.setId(id++);
+	// Changes the normal color
+	args.menuNormalColor && exports.setColorNormal(fam, args.menuNormalColor);
 
-    // Changes the normal color
-    args.menuNormalColor && exports.setColorNormal(fam, args.menuNormalColor);
+	// Changes the pressed color
+	args.menuPressedColor && exports.setColorPressed(fam, args.menuPressedColor);
 
-    // Changes the pressed color
-    args.menuPressedColor && exports.setColorPressed(fam, args.menuPressedColor);
+	args.buttons.forEach(fab => {
+		fam.addButton(exports.createFloatingActionButton(fab));
+	});
 
-    _.each(args.buttons, function(fab) {
-        fam.addButton(exports.createFloatingActionButton(fab));
-    });
-
-    return fam;
-}
-
-exports.setIcon = function(fab, icon, context) {
-    var id = context.getResources().getIdentifier(icon, "drawable", context.getPackageName());
-    fab.setIconDrawable(context.getResources().getDrawable(id, context.getTheme()));
+	return fam;
 };
 
-exports.setColorNormal = function(fab, color) {
-    fab.setColorNormal(Color.parseColor(color));
+exports.setIcon = function (fab, icon, context) {
+	const _id = context.getResources().getIdentifier(icon, 'drawable', context.getPackageName());
+	fab.setIconDrawable(context.getResources().getDrawable(_id, context.getTheme()));
 };
 
-exports.setColorDisabled = function(fab, color) {
-    fab.setColorDisabled(Color.parseColor(color));
+exports.setColorNormal = function (fab, color) {
+	fab.setColorNormal(Color.parseColor(color));
 };
 
-exports.setColorPressed = function(fab, color) {
-    fab.setColorPressed(Color.parseColor(color));
+exports.setColorDisabled = function (fab, color) {
+	fab.setColorDisabled(Color.parseColor(color));
 };
 
-exports.setSize = function(fab, size) {
-    fab.setSize(size);
+exports.setColorPressed = function (fab, color) {
+	fab.setColorPressed(Color.parseColor(color));
+};
+
+exports.setSize = function (fab, size) {
+	fab.setSize(size);
 };
 
 exports.SIZE_MINI = FloatingActionButton.SIZE_MINI;
